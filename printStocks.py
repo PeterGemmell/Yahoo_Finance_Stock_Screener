08f5a1fdfda6ac302b958f3e1c76cdc98ceb6865
Peter Gemmell <petergemmell65@hotmail.com>
2020-07-20 13:32:56 +0100

    screener is searching for stocks without error although awaiting markets to open to clarify

--- printStocks.py
+++ printStocks.py
@@ -1,5 +1,7 @@
 import re
-import urllib2
+# import urllib2
+# import urllib.request
+from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import time
 import datetime
@@ -7,142 +9,131 @@ import datetime
 
 
 def get_stocks(url):
-    print 'Retrieving stock information from ' + url
-    page = urllib2.urlopen(url)
-    soup = BeautifulSoup(page)
+	print ('Retrieving stock information from ' + url)
+	page = urlopen(url)
+	soup = BeautifulSoup(page, 'html.parser') # Added in html.parser here.
 
-    source = str(soup.find_all('table')[5])
-    table = re.findall('><td>(.*?)</td></tr>', source, re.5)
+	source = str(soup.find_all('table')[0])
+	table = re.findall('><td>(.*?)</td></tr>', source, re.S)
 
-    stocks = []
-    # Company name, link, symbol, eps, time
-    pattern = r'(.+)</td><td><a href="(.+)">(.+)</a></td><td align="center">(.+)</td><td align="center"><small>([\w\s]+)</small>'
-    for line in table:
-        M = re.search(pattern, line)
-        if M:
-            stocks.append([M.group(1), M.group(2), M.group(3), M.group(4), M.group(5)])
+	stocks = []
+	# Company name, link, symbol, eps, time
+	pattern = r'(.+)</td><td><a href="(.+)">(.+)</a></td><td align="center">(.+)</td><td align="center"><small>([\w\s]+)</small>'
+	for line in table:
+		M = re.search(pattern, line)
+		if M:
+			stocks.append([M.group(1), M.group(2), M.group(3), M.group(4), M.group(5)])
 
-    return stocks
-
-
+	return stocks
 
 
 def get_next_date():
-    today = datetime.date.today()
-    next = today + datetime.timedelta(days= 7-today.weekday()>3 else 1)
-    next = next.timetuple()
+	today = datetime.date.today()
+	next = today + datetime.timedelta(days= 7-today.weekday() if today.weekday()>3 else 1)
+	next = next.timetuple()
 
-    y = str(next.tm_year)
-    m = next.tm_mon
-    if m <= 9:
-        m = '0' + str(m)
-    else:
-        m = str(m)
-    d = next.tm_mday
-    if d <= 9:
-        d = '0' + str(d)
-    else:
-        d = str(d)
+	y = str(next.tm_year)
+	m = next.tm_mon
+	if m <= 9:
+		m = '0' + str(m)
+	else:
+		m = str(m)
+	d = next.tm_mday
+	if d <= 9:
+		d = '0' + str(d)
+	else:
+		d = str(d)
 
-    date = y + m + d
-    return date
-
+	date = y + m + d
+	return date
 
 
 def screen_positive_eps(stocks):
-    positives = []
-    for stock in stocks:
-        if stock[3] != 'N/A' and float(stock[3]) > 0.0:
-            positives.append(stock)
-
-    return positives
-
+	positives = []
+	for stock in stocks:
+		if stock[3] != 'N/A' and float(stock[3]) > 0.0:
+			positives.append(stock)
+	return positives
 
 
 def sort_today(stocks):
-    sorted = []
-    for stock in stocks:
-        if stock[4] == 'After Market Close':
-            sorted.append(stock)
-    for stock in stocks:
-        if stock[4] == 'Time Not Supplied':
-            sorted.append(stock)
-
-    return sorted
-
+	sorted = []
+	for stock in stocks:
+		if stock[4] == 'After Market Close':
+			sorted.append(stock)
+	for stock in stocks:
+		if stock[4] == 'Time Not Supplied':
+			sorted.append(stock)
+	return sorted
 
 
 def sort_tom(stocks):
-    sorted = []
-    for stock in stocks:
-        if stock[4] == 'Before Market Open':
-            sorted.append(stock)
-    for stock in stocks:
-        if stock[4] == 'Time Not Supplied':
-            sorted.append(stock)
-
-    return sorted
+	sorted = []
+	for stock in stocks:
+		if stock[4] == 'Before Market Open':
+			sorted.append(stock)
+	for stock in stocks:
+		if stock[4] == 'Time Not Supplied':
+			sorted.append(stock)
+	return sorted
 
 
 def stocks_to_text(stocks):
-    text = ''
-    for stock in stocks:
-        text += stock[0][:30]
-        spaces = 30 - len(stock[0])
-        if spaces < 0:
-            spaces = 0
-        text += ' ' * (spaces + 2)
-        text += stock[2][:12]
-        spaces = 13 - len(stock[2])
-        if spaces < 0:
-            spaces = 0
-        text += ' ' * spaces
-        text += stock[3][:4]
-        spaces = 4 - len(stock[3])
-        if spaces < 0:
-            spaces = 0
-        text += ' ' * (spaces+2)
-        text += stock[4]
-        text += '\n'
-    return text
-
+	text = ''
+	for stock in stocks:
+		text += stock[0][:30]
+		spaces = 30 - len(stock[0])
+		if spaces < 0:
+			spaces = 0
+		text += ' ' * (spaces + 2)
+		text += stock[2][:12]
+		spaces = 13 - len(stock[2])
+		if spaces < 0:
+			spaces = 0
+		text += ' ' * spaces
+		text += stock[3][:4]
+		spaces = 4 - len(stock[3])
+		if spaces < 0:
+			spaces = 0
+		text += ' ' * (spaces+2)
+		text += stock[4]
+		text += '\n'
+	return text
 
 
 def print_stock_info():
-    msg = ''
+	msg = ''
 
-    today = 'http://biz.yahoo.com/research/earncal/today.html'
-    stocks = get_stocks(today)
-    stocks = screen_positive_eps(stocks)
-    stocks = sort_today(stocks)
+	today = 'http://biz.yahoo.com/research/earncal/today.html'
+	stocks = get_stocks(today)
+	stocks = screen_positive_eps(stocks)
+	stocks = sort_today(stocks)
 
-    if stocks:
-        msg += 'Today:\n'
-        msg += stocks_to_text(stocks) + '\n'
-    else:
-        msg += 'No positive stocks for today.\n'
+	if stocks:
+		msg += 'Today:\n'
+		msg += stocks_to_text(stocks) + '\n'
+	else:
+		msg += 'No positive stocks for today.\n'
 
-    next_day = get_next_date()
-    next_url = 'http://biz.yahoo.com/research/earncal/' + next_day + '.html'
-    try:
-        stocks = get_stocks(next_url)
-    except:
-        msg += 'Unable to open URL for next day.'
-    else:
-        stocks = screen_positive_eps(stocks)
-        stocks = sort_tom(stocks)
-        if stocks:
-            msg += next_day + ':\n'
-            msg += stocks_to_text(stocks)
-        else:
-            msg += 'No positive stocks for ' + next_day + '\n'
+	next_day = get_next_date()
+	next_url = 'http://biz.yahoo.com/research/earncal/' + next_day + '.html'
+	try:
+		stocks = get_stocks(next_url)
+	except:
+		msg += 'Unable to open URL for next day.'
+	else:
+		stocks = screen_positive_eps(stocks)
+		stocks = sort_tom(stocks)
+		if stocks:
+			msg += next_day + ':\n'
+			msg += stocks_to_text(stocks)
+		else:
+			msg += 'No positive stocks for ' + next_day + '\n'
 
-
-    print ''
-    print msg
-
+	print ('')
+	print (msg)
 
 
 
-if __name__ == '__main__':
-    print_stock_info()
+if __name__ == "__main__":
+	print_stock_info()
